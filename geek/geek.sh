28044bb380e2e3db012182dca6104b4aeca28307
jmontalenti <iacopo.montalenti@autogrill.net>
2016-07-01 11:48:24 +0200

    Create geek.sh

---
+++ geek/geek.sh
@@ -0,0 +1,11 @@
+-----BEGIN GEEK CODE BLOCK-----
+GED/J d-- s:++>: a--
+C++(++++) ULU++ P+ L++
+E---- W+(-) N+++ o+ K+++ w--- O-
+M+ V--
+PS++>$ PE++>$
+Y++ PGP++ t-
+5+++ X++ R+++>$
+TV+ b+ DI+++ D+++ G+++++ e++  h r--
+y++**
+------END GEEK CODE BLOCK------
